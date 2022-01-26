75b7419a6509c36534ffae6786b33a9cada71e2e
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-01-26 12:22:34 +0000

    Basic flask app created, ideal start point for any Flask app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from asyncio import run
 from flask import Flask
 
-#initialise object
+
 app = Flask(__name__)
 
 from controllers import controller
@@ -9,7 +9,7 @@ from controllers import controller
 if __name__ == '__main__':
     app.run(debug=True)
 
-#To run flask server:
+#To run, create an (alias?) flask server:
 # in terminal: export FLASK_APP=app.py
 # flask run
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+To run flask server:
+
+    in terminal: export FLASK_APP=app.py
+    type: flask run
